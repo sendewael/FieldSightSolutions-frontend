9dd6a925d61d97130e3fed26a3038e1693cceeae
Niel Swusten <r0938850@student.thomasmore.be>
2025-01-13 14:29:42 +0100

    registreer knoppen

--- fieldsightsolutions-frontend/src/app/app.component.ts
+++ fieldsightsolutions-frontend/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { LoginButtonComponent } from './Auth0/login-button/login-button.component';
+import { RegisterButtonComponent } from './Auth0/register-button/register-button.component';
+import { LogoutButtonComponent } from './Auth0/logout-button/logout-button.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginButtonComponent, RegisterButtonComponent, LogoutButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ fieldsightsolutions-frontend/src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    AUTH0_DOMAIN: "dev-cs2qfgctjb1eac6u.us.auth0.com",
+    AUTH0_CLIENT_ID: "4prQPOzifuo1Ym3eS122CQlPvOfZNfRx",
+    redirectUri: "http://localhost:4200/callback",
+    home_url: "http://localhost:4200",
+    api_url: "http://localhost:7295/api",
+    AUTH0_AUDIENCE: "https://localhost:7295",
+
+  };
+  
