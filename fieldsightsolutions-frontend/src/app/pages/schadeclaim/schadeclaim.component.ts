6274bdcf4b0e809f1635b7635d817d09577f416e
sendewael <r0856234@student.thomasmore.be>
2025-01-13 11:19:54 +0100

    nav toegevoegd en map en schadeclaims component

---
+++ fieldsightsolutions-frontend/src/app/pages/schadeclaim/schadeclaim.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-schadeclaim',
+  standalone: true,
+  imports: [],
+  templateUrl: './schadeclaim.component.html',
+  styleUrl: './schadeclaim.component.css'
+})
+export class SchadeclaimComponent {
+
+}
